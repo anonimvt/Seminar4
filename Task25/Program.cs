9c00df3ad3145a22da36ac60bb5602e97f42354c
anonimvt <75793665+anonimvt@users.noreply.github.com>
2023-02-13 10:59:49 +0400

    Commit 1 Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,22 @@
+﻿int numberA = InputInt("Введите число: ");
+int numberB = InputInt("Введите степень: ");
+Stepen(numberA, numberB);
+
+
+// Функция возведения в степень
+void Stepen(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(a + " в " + b + "степени равно: " + result);
+}
+
+// Функция ввода данных с предварительном выводом сообщения
+int InputInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
